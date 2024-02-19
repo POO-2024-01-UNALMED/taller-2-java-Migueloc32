e3c48abe95adac55871301eba5136ce2626e1a7b
Migueloc32 <159493278+Migueloc32@users.noreply.github.com>
2024-02-19 17:11:34 -0500

    Primera subida

---
+++ src/test/java/test/Auto.java
@@ -0,0 +1,35 @@
+package test;
+
+public class Auto{
+    String modelo;
+    int precio;
+    Asiento [] asientos;
+    String marca;
+    Motor motor;
+    int registro;
+    static int cantidadCreados;
+
+    int cantidadAsientos(){
+    int totalAsientos=0;
+    for(int i=0, asientos.length, i++){
+        if(asientos[i]!=null){
+            totalAsientos++
+        }    
+    }
+    return totalAsientos;
+    }
+    String verificarIntegridad(){
+        if (this.motor.registro==this.registro){
+            for (int i=0, asientos.length, i++){
+                if(this.asientos[i]!=null){
+                    if(this.asientos[i].registro !=this.registro){
+                        return "Las piezas no son originales"
+                    }
+                }
+            }
+            return "Auto original"
+        }
+        return "Las piezas no son originales"
+    }
+
+}
