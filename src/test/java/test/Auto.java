9cdc46938c2bd1d9927ce09009d929535ecd8fc5
Migueloc32 <159493278+Migueloc32@users.noreply.github.com>
2024-02-19 17:35:25 -0500

    Segunda entrega

--- src/test/java/test/Auto.java
+++ src/test/java/test/Auto.java
@@ -11,25 +11,25 @@ public class Auto{
 
     int cantidadAsientos(){
     int totalAsientos=0;
-    for(int i=0, asientos.length, i++){
+    for(int i=0; i<asientos.length ; i++){
         if(asientos[i]!=null){
-            totalAsientos++
+            totalAsientos++;
         }    
     }
     return totalAsientos;
     }
     String verificarIntegridad(){
         if (this.motor.registro==this.registro){
-            for (int i=0, asientos.length, i++){
+            for (int i=0; i<asientos.length; i++){
                 if(this.asientos[i]!=null){
                     if(this.asientos[i].registro !=this.registro){
-                        return "Las piezas no son originales"
+                        return "Las piezas no son originales";
                     }
                 }
             }
-            return "Auto original"
+            return "Auto original";
         }
-        return "Las piezas no son originales"
+        return "Las piezas no son originales";
     }
 
 }
